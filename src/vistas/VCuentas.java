ebf84f368465c358340b3da58e6e8f96926373ee
eduardo19Fu <ramieduar@gmail.com>
2018-09-25 09:49:33 -0600

    Agregación de Multicatch en interfaz gráfica

--- src/vistas/VCuentas.java
+++ src/vistas/VCuentas.java
@@ -242,13 +242,7 @@ public class VCuentas extends javax.swing.JFrame {
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(VCuentas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(VCuentas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(VCuentas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(VCuentas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
@@ -259,10 +253,8 @@ public class VCuentas extends javax.swing.JFrame {
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
         }
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new VCuentas().setVisible(true);
-            }
+        java.awt.EventQueue.invokeLater(() -> {
+            new VCuentas().setVisible(true);
         });
     }
 
